2d112fb02d4d779d98a95c3b10dd200eaeaf8b33
changchangwoo <changchangwoo@naver.com>
2024-09-10 22:28:29 +0900

    Chore : svg 이미지 받기 위한 타입스크립트 외부 컴포넌트(dotlottie-player) 허용

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import React, { ReactNode } from "react";
 
 interface LayoutProps {
@@ -13,6 +14,22 @@ const Layout = ({ children, title }: LayoutProps) => {
         <h1 className="text-pureWhite text-lg font-bold">{title}</h1>
       </div>
       {children}
+      <div className="w-full flex justify-center items-center">
+      <Script
+          src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+          type="module"
+          strategy="afterInteractive"
+        />
+        <dotlottie-player
+          src="https://lottie.host/e300cb38-7d84-4ffc-a5f5-3e79d62df193/qMcWoF9z0E.json"
+          background="transparent"
+          speed="1"
+          direction="1"
+          style={{ width: '50px', height: '50px' }}
+          loop
+          autoplay
+        ></dotlottie-player>
+        </div>
     </div>
   );
 };
--- src/components/overview/OverView.tsx
+++ src/components/overview/OverView.tsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import Layout from '../common/Layout'
-import { SECTION_TITLE } from '@/constants/constants'
-import HighlightText from '../common/HighlightText'
+import React from "react";
+import Layout from "../common/Layout";
+import { SECTION_TITLE } from "@/constants/constants";
+import HighlightText from "../common/HighlightText";
 
 const OverView = () => {
   return (
     <Layout title={SECTION_TITLE.overview}>
-      <div className='w-full h-full flex justify-center items-center flex-col'>
-        <div className='w-full'>
-        <h1 className='text-[96px] text-grey font-bold leading-6'>FRONT-END</h1>
-        <h2 className='text-[64px] text-pureWhite'>프론트엔드 개발자 <HighlightText>이창우</HighlightText></h2>
-        <hr className="flex-grow border-solid border-2 border-pureWhite my-10" />
-        <h3 className='text-2xl text-pureWhite'>
-          커피한잔과의 <HighlightText>사색을 즐기며</HighlightText>,<br/>
-        하나의 문제를 <HighlightText>다양한 시각에서 접근</HighlightText>하는 개발자입니다
-        .</h3>
-
+      <div className="w-full h-full flex justify-center items-center flex-col">
+        <div className="w-full">
+          <h1 className="text-[96px] text-grey font-bold leading-6">
+            FRONT-END
+          </h1>
+          <h2 className="text-[64px] text-pureWhite">
+            프론트엔드 개발자 <HighlightText>이창우</HighlightText>
+          </h2>
+          <hr className="flex-grow border-solid border-2 border-pureWhite my-10" />
+          <h3 className="text-2xl text-pureWhite">
+            커피 한 잔과의 <HighlightText>사색을 즐기며</HighlightText>,<br />
+            하나의 문제를 <HighlightText>다양한 시각에서 접근</HighlightText>
+            하는 개발자입니다
+          </h3>
         </div>
-        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default OverView
+export default OverView;
